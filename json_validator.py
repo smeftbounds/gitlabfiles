e291c66918c59d9b6a83f9460cf3bce888e5fc73
Karolos Potamianos <karolos.potamianos@cern.ch>
2025-10-02 16:12:56 +0200

    Updating validator to compare operators from WCxF basis definitions

--- json_validator.py
+++ json_validator.py
@@ -27,9 +27,50 @@ def validate_json(in_file, verbose=True):
 
         return True
 
+wcxf_basis_data = {}
+wcxf_valid_ops = {}
+
+def wcxf_load_basis(in_file):
+    print("Loading WCxF basis definitions")
+    with open(in_file) as f:
+        import json
+        json_data = json.load(f)
+        eft = json_data["eft"]
+        basis = json_data["basis"]
+        if eft not in wcxf_basis_data:
+            wcxf_basis_data[eft] = {}
+            wcxf_valid_ops[eft] = {}
+        wcxf_basis_data[eft][basis] = json_data
+        wcxf_valid_ops[eft][basis] = [ k 
+                      for sector in wcxf_basis_data[eft][basis]["sectors"].keys() 
+                      for k in wcxf_basis_data[eft][basis]["sectors"][sector].keys() ]
+        print(f"Found {len(wcxf_valid_ops[eft][basis])} valid operators for {eft} {basis}")
+
+def wcxf_check_operators(json_data):
+    allgood = True
+    for result in json_data["results"]:
+        eft = result["wcxf_eft"]
+        basis = result["wcxf_basis"]
+        for c in result["bounds_coeffs"]:
+            if c["coeff"] not in wcxf_valid_ops[eft][basis]:
+                print(f"ERROR: operator {c['coeff']} isn't a valid name in WCxf {eft} {basis} definition.")
+                allgood = False
+    return allgood
+
+def wcxf_validate_json(json_to_validate):
+    wcxf_check_operators(json_to_validate)
+
 # This is to add extra high-level validation that cannot easily be put into the schema
 # Note that the alternative approach might be to augment the schema scriptically
 def validate_content(in_file, verbose=True):
+    if not wcxf_basis_data:
+        import os, glob, wcxf
+        for f in glob.glob(os.path.dirname(wcxf.__file__)+"/**/*.basis.json", recursive=True):
+            wcxf_load_basis(f)
+    with open(in_file) as f:
+        import json
+        json_to_validate = json.load(f)
+        wcxf_validate_json(json_to_validate)
     return True
 
 if __name__ == '__main__':
@@ -37,6 +78,7 @@ if __name__ == '__main__':
     allgood = True
     for f in sys.argv[1:]:
         if not validate_json(f): allgood = False
+        if validate_content(f): allgood = False
     if allgood == False:
         print("ERROR: some of the files are not valid according to the schema. Plase fix it or contact the authors.")
     sys.exit(not allgood)
